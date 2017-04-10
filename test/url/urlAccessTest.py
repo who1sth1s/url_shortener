6f5c9d755e612ed53e8ae9234b2b2e435dfd6ab8
Who1sth1s <ksjlike69@gmail.com>
2017-04-11 02:41:31 +0900

    Modify test code to success case and fail case

--- test/url/urlAccessTest.py
+++ test/url/urlAccessTest.py
@@ -1,17 +1,24 @@
-import unittest, ujson
+import unittest
 
 from service.url.urlAccess import UrlAccess
 
 
 class UrlStatsTest(unittest.TestCase):
 
-    def test_get(self):
-        request = {
+    def setUp(self):
+        self.request = {
             'method': 'GET'
         }
-        url_number = 123123
-        result = UrlAccess(request, url_number).execute()
-        print(result)
+
+    def fail_test_get(self):
+        url_number = 1223123
+        result = UrlAccess(self.request, url_number).execute()
+        self.assertEqual(result.get('error') is not None, True)
+
+    def success_test_get(self):
+        url_number = 7
+        result = UrlAccess(self.request, url_number).execute()
+        self.assertEqual(result.get('registered_url') is not None, True)
 
 if __name__ == '__main__':
     unittest.main()
--- test/url/urlRegisterTest.py
+++ test/url/urlRegisterTest.py
@@ -1,4 +1,4 @@
-import unittest, ujson
+import unittest
 
 from service.url.urlRegister import UrlRegister
 
@@ -11,9 +11,14 @@ class UrlRegisterTest(unittest.TestCase):
             'url': 'http://www.google.co.kr'
         }
 
-    def test_post(self):
+    def test_post_duplicate_success(self):
         result = UrlRegister(self.post_request).execute()
-        print(ujson.loads(result))
+        self.assertEqual(result[1], 200)
+
+    def test_post_success(self):
+        self.post_request['url'] = 'http://123.123.1213'
+        result = UrlRegister(self.post_request).execute()
+        self.assertEqual(result[1], 201)
 
     def test_get_duplicate_registered_url_method(self):
         result = UrlRegister(self.post_request)._get_duplicate_registered_url('http://www.vingle.net')
